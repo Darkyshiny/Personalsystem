44a6d2070bfe00515cae66c933ac0e9c669aaa33
Darkyshiny <darkyshiny@hotmail.com>
2016-01-20 09:29:11 +0100

    Models (incomplete)

---
+++ Personalsystem/Personalsystem/Models/Group.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Personalsystem.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public List<ApplicationUser> Employees { get; set; }
+    }
+}
---
+++ Personalsystem/Personalsystem/Models/Vacancy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Personalsystem.Models
+{
+    public class Vacancy
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+    }
+}
