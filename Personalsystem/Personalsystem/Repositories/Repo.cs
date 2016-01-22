5233d884ec0abdf0dbfd34bbed4585c806b0f705
Darkyshiny <darkyshiny@hotmail.com>
2016-01-22 08:48:15 +0100

    asd

--- Personalsystem/Personalsystem/Repositories/Repo.cs
+++ Personalsystem/Personalsystem/Repositories/Repo.cs
@@ -1,19 +1,34 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Personalsystem.DataAccessLayer;
 using Personalsystem.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 
 namespace Personalsystem.Repositories
 {
     public class Repo
     {
-        
-        public void SetUserRole()
+        private PersonalSystemContext db = new PersonalSystemContext();
+
+
+        public void SetUserRole(string roleName, string userName)
         {
-            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>());
+            UserManager<ApplicationUser> UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            RoleManager<IdentityRole> RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+
+            var user = UserManager.FindByName(userName);
+            var rolesForUser = UserManager.GetRoles(user.Id);
+
+            if (!rolesForUser.Contains(roleName))
+            {
+                UserManager.AddToRole(user.Id, roleName);
+            }
+
         }
     }
 }
