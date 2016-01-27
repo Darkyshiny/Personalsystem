166e79914adfc428ea40c091f2c0a5becf8398f4
Davaeorn <davaeorn@gmail.com>
2016-01-27 11:27:40 +0100

    restored vacancy (merge should be ok now)

---
+++ Personalsystem/Personalsystem/Controllers/VacancyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Personalsystem.Models;
+using Personalsystem.DataAccessLayer;
+
+namespace Personalsystem.Controllers
+{
+    public class VacancyController : Controller
+    {
+        private PersonalSystemContext db = new PersonalSystemContext();
+        private Vacancy repo = new Vacancy();
+
+        // GET: Vacancy
+        public ActionResult Index()
+        {
+            var companyId = db.company.Find(1);
+            var vacancy = repo.ListVacancies(companyId);
+            return View(vacancy);
+        }
+    }
+}
--- Personalsystem/Personalsystem/Models/Vacancy.cs
+++ Personalsystem/Personalsystem/Models/Vacancy.cs
@@ -12,7 +12,42 @@ namespace Personalsystem.Models
         public int Id { get; set; }
         public string Description { get; set; }
         public int Did { get; set; }
-        [ForeignKey("Did")]
+        [ForeignKey("dId")]
         public virtual Department Department { get; set; }
+        public int Cid { get; set; }
+        [ForeignKey("cId")]
+        public virtual Company Company { get; set; }
+
+        private PersonalSystemContext db = new PersonalSystemContext();
+
+        public List<Vacancy> ListVacancies(Company company)
+        {
+            var vacancies = db.vacancy.ToList();
+            var result = vacancies.Where(v => v.Cid == company.Id).ToList();
+            if (result.Count > 0)
+            {
+                return result.OrderBy(v => v.Id).ToList();
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        public List<Vacancy> ListVacancies(Company company, string search)
+        {
+            var vacancies = db.vacancy.ToList();
+            var result = vacancies.Where(v => v.Cid == company.Id).ToList();
+            var searchString = result.Where(r => r.Description.Contains(search)).ToString();
+
+            if (result.Count > 0 & searchString.Contains(search))
+            {
+                return result.OrderBy(v => v.Id).ToList();
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
     }
 }
