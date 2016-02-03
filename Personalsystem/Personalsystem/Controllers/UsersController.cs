155691bcedb3f22357aa0776616138d1bc9884b4
scFathi <aayy1347@gmail.com>
2016-02-03 09:59:17 +0100

    user controller for delete  user by searching id

--- Personalsystem/Personalsystem/Controllers/UsersController.cs
+++ Personalsystem/Personalsystem/Controllers/UsersController.cs
@@ -8,12 +8,15 @@ using System.Web;
 using System.Web.Mvc;
 using Personalsystem.DataAccessLayer;
 using Personalsystem.Models;
+using Personalsystem.Repositories;
 
 namespace Personalsystem.Controllers
 {
     public class UsersController : Controller
     {
         private PersonalSystemContext db = new PersonalSystemContext();
+        private Repo repo = new Repo();
+       
 
         // GET: Users/Details/5
         public ActionResult Details(string id)
@@ -38,5 +41,75 @@ namespace Personalsystem.Controllers
             }
             base.Dispose(disposing);
         }
+//Begin, written by Ali ********************************************************
+        public ActionResult Index()
+        {
+            var user = db.user;
+            return View(user.ToList());
+        }
+
+
+        [HttpPost]
+        public ActionResult FindUserByUserName(string search)
+        {
+            if (search != null)
+            {
+                //var re = repo.FindPersonalsBySearchId(search);
+                search = search.Trim();
+                var re = repo.FindPersonalsBySearchUserName(search);
+                return View("Index", re.ToList());
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult FindUserByUserId(string search)
+        {
+            search = search.Trim();
+            if (search != null)
+            {
+                var re = repo.FindPersonalsBySearchId(search);
+
+                //var re = repo.FindPersonalsBySearchUserName(search);
+                return View("Index", re.ToList());
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        // GET: ApplicationUsers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = db.user.Find(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationUser);
+        }
+        // POST: ApplicationUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ApplicationUser applicationUser = db.user.Find(id);
+            db.user.Remove(applicationUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    
+//********************************************************************
+
+
     }
 }
